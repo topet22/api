3e20e1a6d3f74293b105959dfdb3eabc114cd786
JPumares024 <pumaresjomar0824@gmail.com>
2022-11-19 13:58:33 +0800

    Database file import niyo na lng sa sqlyog niyo

---
+++ dms.sql
@@ -0,0 +1,56 @@
+/*
+SQLyog Community v13.1.8 (64 bit)
+MySQL - 10.4.21-MariaDB : Database - dms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dms` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `dms`;
+
+/*Table structure for table `documents` */
+
+DROP TABLE IF EXISTS `documents`;
+
+CREATE TABLE `documents` (
+  `document_id` int(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  `origin` varchar(100) DEFAULT NULL,
+  `date_received` datetime DEFAULT NULL,
+  `destination` varchar(100) DEFAULT NULL,
+  `tags` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`document_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `documents` */
+
+/*Table structure for table `user_account` */
+
+DROP TABLE IF EXISTS `user_account`;
+
+CREATE TABLE `user_account` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `user_account` */
+
+insert  into `user_account`(`id`,`name`,`email`,`password`) values 
+(11,'Jomar Pumares','jomar.pumares_2@student.dmmmsu.edu.ph','aaaa');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
