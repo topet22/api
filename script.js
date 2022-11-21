2ac59676d865f94335a3d8c51f4f76f4173b5bc4
ned-rev <denverordinante@gmail.com>
2022-11-21 10:42:18 +0700

    updated jquery script by records team

--- script.js
+++ script.js
@@ -1,110 +1,105 @@
 $(document).ready(function(){
-	$("#save").click(function(){
-		var lname=$("#lname").get(0).value;
-		var fname=$("#fname").get(0).value;
-		$.post("http://www.localhost/api/public/postName",
+
+    $("#login").click(function(){
+		var uname=$("uname").get(0).value;
+		var passkey=$("passkey").get(0).value;
+		
+		$.post("http://www.localhost/api/public/loginUser",
 				JSON.stringify({
-				lname: lname,
-				fname: fname
+				uname: uname,
+				passkey: passkey
 				}),
 			function(data,status){
 			alert("Data: " + data + "\nStatus: " + status);
 		});
 	});
-$("#display").click(function(){
-	$.post("http://localhost/api/public/postPrint",
-	function(data, status){
-		var json=JSON.parse(data);
-		var row="";
-		for(var i=0;i<json.data.length;i++){
 
-		row=row+"<tr><td>"+json.data[i].lname+"</td><td>"+json.data[i].fname+"</td></tr>";
 
-			}
-			$("#data").get(0).innerHTML=row;
-		});
-	});	
-$("#search").click(function(){
-	id=prompt("Enter Name ID");
-//endpoint
-	$.post("http://localhost/api/public/searchName",
-	JSON.stringify(
-//payload
-					  {
-					    id:id
-					  }
-				  ),
-function(data, status){
-//result
-var json=JSON.parse(data);
-$("#fname").get(0).value=json.data[0].fname;
-$("#lname").get(0).value=json.data[0].lname;
-console.log(json);
-
-});
-});
-$("#update").click(function(){
-	var fname=$("#fname").get(0).value;
-	var lname=$("#lname").get(0).value;
-	$.post("http://localhost/api/public/updateName",
-	JSON.stringify({
-	id:id,
-	lname:lname,
-	fname:fname
-	}),
-function(data, status){
-alert("Data: " + data + "\nStatus: " + status);
-
-});
-});
-$("#delete").click(function(){
-	$.post("http://localhost/api/public/deleteName",
-	JSON.stringify({
-	id:id
-	}),
-	function(data, status){
-
-	alert("Data: " + data + "\nStatus: " + status);
-
-	});
-	});
-
-    $('#password, #confirm_password').on('keyup', function () {
-		$('#butsave').prop('disabled',true);
-		if (($('#password').val() == $('#confirm_password').val()) && $('#password').val() != "") {
-		  $('#message').html('Matching').css('color', 'green');
-		  $('#butsave').prop('disabled',false);
-		  
-		} else 
-		  $('#message').html('Not Matching').css('color', 'red');		  
-	  });
-
-      $("#butsave").click(function(){
-		var name=$("#name").get(0).value;
-		var username=$("#username").get(0).value;
-        var password=$("#password").get(0).value;
-		$.post("http://www.localhost/api/public/userreg",
+    //CREATE FILE UPLOAD
+    $("#save").click(function(){
+		var document_TITLE=$("document_TITLE").get(0).value;
+		var document_TYPE=$("document_TYPE").get(0).value;
+		var document_ORIGIN=$("document_ORIGIN").get(0).value;
+		var date_received=$("date_received").get(0).value;
+		var document_DESTINATION=$("document_DESTINATION").get(0).value;
+		var tags=$("tags").get(0).value;
+		
+		$.post("http://www.localhost/api/public/fileupload",
 				JSON.stringify({
-				name: name,
-				username: username,
-                password: password
+				document_TITLE: document_TITLE,
+				document_TYPE: document_TYPE,
+				document_ORIGIN: document_ORIGIN,
+				date_received: date_received,
+				document_DESTINATION: document_DESTINATION,
+				tags: tags
 				}),
 			function(data,status){
 			alert("Data: " + data + "\nStatus: " + status);
 		});
 	});
-	
-});
 
-function checkUsername() { //Register JS
+    //Update File Upload
+    $("#update").click(function(){
+        var document_ID=$("document_ID").get(0).value;
+		var document_TITLE=$("document_TITLE").get(0).value;
+		var document_TYPE=$("document_TYPE").get(0).value;
+		var document_ORIGIN=$("document_ORIGIN").get(0).value;
+		var date_received=$("date_received").get(0).value;
+		var document_DESTINATION=$("document_DESTINATION").get(0).value;
+		var tags=$("tags").get(0).value;
+		
+		$.post("http://www.localhost/api/public/updatefileupload",
+				JSON.stringify({
+                document_ID: document_ID,
+                document_TITLE: document_TITLE,
+				document_TYPE: document_TYPE,
+				document_ORIGIN: document_ORIGIN,
+				date_received: date_received,
+				document_DESTINATION: document_DESTINATION,
+				tags: tags
+				}),
+			function(data,status){
+			alert("Data: " + data + "\nStatus: " + status);
+		});
+	});
+
+    //Search File Upload
+
+    $("#search").click(function(){
+        document_ID=prompt("Enter Name ID");
+    //endpoint
+        $.post("http://localhost/api/public/searchfileupload",
+        JSON.stringify(
+    //payload
+                          {
+                            document_ID:document_ID
+                          }
+                      ),
+    function(data, status){
+    //result
+    var json=JSON.parse(data);
+    $("document_ID").get(0).value=json.data[0].fname;
+    $("document_TITLE").get(0).value=json.data[0].fname;
+    $("document_TYPE").get(0).value=json.data[0].fname;
+    $("document_ORIGIN").get(0).value=json.data[0].fname;
+    $("date_received").get(0).value=json.data[0].fname;
+    $("document_DESTINATION").get(0).value=json.data[0].fname;
+    $("tags").get(0).value=json.data[0].fname;    
+    console.log(json); 
+    });
+    });
+
+    //Delete File Upload
+    $("#delete").click(function(){
+        $.post("http://localhost/api/public/deleteName",
+        JSON.stringify({
+        document_ID:document_ID
+        }),
+        function(data, status){
     
-	jQuery.ajax({
-	url: "php/usernamevalidation.php",
-	data:'username='+$("#username").val(),
-	type: "POST",
-	success:function(data){
-		$("#check-username").html(data);
-	},
-	error:function (){}
-	});
-}
+        alert("Data: " + data + "\nStatus: " + status);
+    
+        });
+        });
+});
+
